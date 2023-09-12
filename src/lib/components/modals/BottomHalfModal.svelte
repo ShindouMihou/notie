7a2ddb4af3a5e90a482ba6c1722b21b93f883614
Shindou Mihou <git@mihou.pw>
2023-09-12 11:39:43 +0800

    feat: add drag none  to bottom half  modal

--- src/lib/components/modals/BottomHalfModal.svelte
+++ src/lib/components/modals/BottomHalfModal.svelte
@@ -5,10 +5,20 @@
     const dispatcher = createEventDispatcher()
 </script>
 
+<style>
+    .drag-none {
+        -webkit-user-drag: none;
+        -khtml-user-drag: none;
+        -moz-user-drag: none;
+        -o-user-drag: none;
+        user-drag: none;
+    }
+</style>
+
 {#if show}
-    <div class="fixed z-30 animate-in fade-in duration-300 transition ease-in-out bottom-0 left-0 select-none">
-        <button class="min-h-screen bg-black bg-opacity-40 w-screen" on:click={() => dispatcher('hide')}></button>
-        <div class="min-h-fit bg-[#2b2b2b] bg-opacity-90 shadow backdrop-blur border-t border-l border-r border-[#3a3a3a] border-opacity-30 p-12 pt-12 rounded-t-xl fixed w-screen bottom-0 left-0 animate-in slide-in-from-bottom transition ease-in-out">
+    <div class="fixed z-30 animate-in fade-in duration-300 transition ease-in-out bottom-0 left-0 select-none drag-none">
+        <button class="min-h-screen bg-black bg-opacity-40 w-screen select-none drag-none" on:click={() => dispatcher('hide')}></button>
+        <div class="min-h-fit bg-[#2b2b2b] bg-opacity-90 shadow backdrop-blur drag-none border-t border-l border-r border-[#3a3a3a] border-opacity-30 p-12 pt-12 rounded-t-xl fixed w-screen bottom-0 left-0 animate-in slide-in-from-bottom transition ease-in-out">
             <div class="flex flex-col" id="modal-content">
                 <h1 class="font-semibold text-3xl lowercase">Notie<span class="text-blue-400">.</span></h1>
                 <slot/>
